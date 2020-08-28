97484cf9838ee23bca2b2463ebb45e759ddf4927
amanueladdisu <amaniaddisu60@gmail.com>
2020-08-28 11:46:36 -0500

    Update multiply.js

--- assignment_6/multiply.js
+++ assignment_6/multiply.js
@@ -1,13 +1,16 @@
 'use strict';
 function multiply(list,multiplier){
 let output=[];
-    for(let i=0;i<list.length;i++){
-        output.push(list[i]*multiplier);
+    // for(let i=0;i<list.length;i++){                          //this one also works  
+    //     output.push(list[i]*multiplier);
+    // }
+    for(let n of list){
+        output.push(n*multiplier);
     }
     return output;
 }
 
 let list =[17,8,9,5,20,80];
-let products = multiply(list,3);
+let products = multiply(list,2);
 console.log(products);
 //console.log(list.map(n=>n*3));
