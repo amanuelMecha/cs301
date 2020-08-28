390f6c20d3de9c6ae5beb5b62364f71108cc5a6b
amanueladdisu <amaniaddisu60@gmail.com>
2020-08-28 10:02:16 -0500

    Update christmassGift.js

--- assignment_6/christmassGift.js
+++ assignment_6/christmassGift.js
@@ -1,10 +1,11 @@
 'use strict';
-let days = [1,2,3,4,5,6,7,8,9,10,11,12];
+let days = ["first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth"];
 let gifts = ["a partridge in a pear tree","turtle doves","French hens","calling birds","golden rings","geese a laying","swans a swimming","maids a milking","ladies dancing",
 "lords a leaping","pipers piping","drummers drumming"];
 let input =require('prompt-sync')();
 let day = parseInt(input("Enter an integer b/n 1 to 12: "));
 let x =day-1;
+let  y = days[x];
 
 function christmassGift(){
     let z=[];
@@ -12,6 +13,6 @@ function christmassGift(){
         z.push(gifts[i]);
     }
 
-        return ("my true love gave to me :" + z.join(","));
+        return ("On the "+ y+ "day of Christmas, my true love gave to me :" + z.join(","));
 }
 console.log(christmassGift());
